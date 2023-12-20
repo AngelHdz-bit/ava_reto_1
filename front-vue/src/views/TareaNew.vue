9c1e995625b65433a8f6a8d4655560be0fc1314d
AngelHdz-bit <57919666+AngelHdz-bit@users.noreply.github.com>
2023-12-20 09:39:59 -0600

    Creación de la vista para ver las tareas

--- front-vue/src/views/TareaNew.vue
+++ front-vue/src/views/TareaNew.vue
@@ -1,6 +1,31 @@
 <template>
-    <div class="about">
-      <h1>Crear tarea</h1>
-    </div>
+    <html lang="en">
+        <div class="container">
+            <a href="" class="btn btn-outline-primary mt-4"> Agregar</a>
+            <table class="table table-bordered table-striped text-center mt-4">
+                <thead>
+                    <tr class="bg-primary text-white">
+                        <th scope="col">ID</th>
+                        <th scope="col" >Titulo</th>
+                        <th scope=" col" >Descripcion</th>
+                        <th scope=" col" >Completada</th>
+                        <th scope=" col" >Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Comprar viveres</td>
+                        <td>Ir al supermercado</td>
+                        <td>Si</td>
+                        <td>
+                            <a href="" class="btn btn-outline-info">editar</a>
+                            <a href="" class="btn btn-outline-danger">borrar</a>
+                        </td>
+                     </tr>
+                </tbody>
+            </table>
+        </div>
+    </html>
 </template>
   
