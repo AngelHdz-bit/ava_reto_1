b2a5a58e182733a342a28b9202e5c19873b69019
AngelHdz-bit <57919666+AngelHdz-bit@users.noreply.github.com>
2023-12-20 10:14:55 -0600

    Uso de axios para consumir el endpoint para mostrar las tareas

--- front-vue/src/views/TareaNew.vue
+++ front-vue/src/views/TareaNew.vue
@@ -7,25 +7,50 @@
                     <tr class="bg-primary text-white">
                         <th scope="col">ID</th>
                         <th scope="col" >Titulo</th>
-                        <th scope=" col" >Descripcion</th>
-                        <th scope=" col" >Completada</th>
-                        <th scope=" col" >Acciones</th>
+                        <th scope="col" >Descripcion</th>
+                        <th scope="col" >Completada</th>
+                        <th scope="col" >Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Comprar viveres</td>
-                        <td>Ir al supermercado</td>
-                        <td>Si</td>
+                    <tr v-for="tarea in tareas" :key="tarea.id">
+                        <td v-text="tarea.id"></td>
+                        <td v-text="tarea.titulo"></td>
+                        <td v-text="tarea.descripcion"></td>
+                        <td v-text="tarea.completada"></td>
                         <td>
                             <a href="" class="btn btn-outline-info">editar</a>
                             <a href="" class="btn btn-outline-danger">borrar</a>
                         </td>
-                     </tr>
+                    </tr>
                 </tbody>
             </table>
         </div>
     </html>
 </template>
-  
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data() {
+            return {
+                tareas: null,
+            };
+        },
+        mounted() {
+            this.getTareas(); // Llama a la función getTareas
+        },
+        methods: {
+            getTareas() {
+                axios.get('http://127.0.0.1:8000/api/get-tareas')
+                    .then(res => {
+                        this.tareas = res.data;
+                    })
+                    .catch(error => {
+                        console.error('Error al obtener las tareas', error);
+                    });
+            },
+        },
+    };
+</script>
