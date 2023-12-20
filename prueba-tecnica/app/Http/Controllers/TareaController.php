86a97bf0b2efd1bcaa5ce39f4a953ee896c5517b
AngelHdz-bit <57919666+AngelHdz-bit@users.noreply.github.com>
2023-12-19 20:00:02 -0600

    Creación del metodo store para agregar tareas

---
+++ prueba-tecnica/app/Http/Controllers/TareaController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tarea;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Database\QueryException;
+class TareaController extends Controller
+{
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'titulo' => 'required|string|max:255',
+                'descripcion' => 'required|string',
+                'completada' => 'required|string',
+            ]);
+            $tarea = new Tarea($request->all());
+            $tarea->save();
+            return response()->json(['message' => 'Tarea creada correctamente'], 201);
+        } catch (QueryException $e) {
+            return response()->json(['error' => 'Error al crear la tarea: ' . $e->getMessage()], 500);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Error inesperado al crear la tarea: ' . $e->getMessage()], 500);
+        }
+    }
+
+}
